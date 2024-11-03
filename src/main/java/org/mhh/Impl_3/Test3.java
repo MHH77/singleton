6335d94ecd0e930625d8053d1db8bb4b4a5529ce
MHH77 <mesfandiari77@gmail.com>
2024-11-02 22:14:32 -0700

    [feature] implement as synchronized

---
+++ src/main/java/org/mhh/Impl_3/Test3.java
@@ -0,0 +1,21 @@
+package org.mhh.Impl_3;
+
+public class Test3 {
+    private Test3() {
+    }
+
+    private static Test3 instance;
+
+    public synchronized static Test3 getInstance() {
+        if (instance == null) {
+            instance = new Test3();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Test3.getInstance());
+
+    }
+
+}
