4ae15a443c842d84f8ce6df5d90c6630d1db8b6a
MHH77 <mesfandiari77@gmail.com>
2024-11-02 22:10:16 -0700

    [feature] implement as Eager Initialization

---
+++ src/main/java/org/mhh/Impl_1/Test1.java
@@ -0,0 +1,17 @@
+package org.mhh.Impl_1;
+
+public class Test1 {
+    private Test1() {
+    }
+
+    private static final Test1 instance = new Test1();
+
+    public static Test1 getInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Test1.getInstance());
+    }
+
+}
